6cea51bae196572be5cac2a9fc47b2322aea9059
carlos (el-cajon) <padi_05@yahoo.com>
2015-10-12 13:38:26 -0400

    Removed useless scripts.

--- before-shutdown.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-mysqldump -urss -prss rss | gzip > "$DROPBOX_DIR/Environment/rss/rss.sql.gz"
--- rss_import.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-zcat "$DROPBOX_DIR/Environment/rss/rss.sql.gz" | mysql -urss -prss rss
